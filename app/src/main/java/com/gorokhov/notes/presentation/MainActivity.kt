0cbb681e2bcbb9c8b51609eb69ffd359893c2213
Anton <bonduelle85@gmail.com>
2026-04-05 21:21:02 +0300

    LazyLists

--- app/src/main/java/com/gorokhov/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/gorokhov/notes/presentation/MainActivity.kt
@@ -11,37 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gorokhov.notes.presentation.screens.notes.NotesScreen
 import com.gorokhov.notes.presentation.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             NotesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NotesScreen(
+                    modifier = Modifier.padding(top = 34.dp)
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotesTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/gorokhov/notes/presentation/screens/notes/NotesScreen.kt
@@ -0,0 +1,75 @@
+package com.gorokhov.notes.presentation.screens.notes
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.gorokhov.notes.domain.Note
+
+@Composable
+fun NotesScreen(
+    modifier: Modifier,
+    viewModel: NotesViewModel = viewModel()
+) {
+
+    val state by viewModel.state.collectAsState()
+
+    LazyColumn {
+
+        item {
+            LazyRow(
+                modifier = modifier
+            ) {
+                items(
+                    items = state.pinnedNotes,
+                    key = { note -> note.id }
+                ) { note ->
+                    NoteCard(
+                        note = note,
+                        onClick = {
+                            viewModel.processCommand(
+                                NotesCommand.SwitchPinStatus(note.id)
+                            )
+                        }
+                    )
+                }
+            }
+        }
+
+        items(
+            items = state.otherNotes,
+            key = { note -> note.id }
+        ) { note ->
+            NoteCard(
+                note = note,
+                onClick = {
+                    viewModel.processCommand(
+                        NotesCommand.SwitchPinStatus(note.id)
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun NoteCard(
+    modifier: Modifier = Modifier,
+    note: Note,
+    onClick: (Note) -> Unit
+) {
+    Text(
+        text = "${note.title} - ${note.content}",
+        modifier = modifier
+            .clickable { onClick(note) }
+            .padding(16.dp)
+    )
+}
